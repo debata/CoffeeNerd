554130be04e52129d88d1d6d17dbce6f80eb1e5d
Daryl Ebata <hashbangsoftware@gmail.com>
2014-02-03 17:25:10 -0500

    Added the btc wallet to the about page

--- CoffeePro/src/com/hashbang/coffeenerd/AboutFragment.java
+++ CoffeePro/src/com/hashbang/coffeenerd/AboutFragment.java
@@ -2,9 +2,12 @@ package com.hashbang.coffeenerd;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
@@ -15,6 +18,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.hashbang.coffeepro.R;
 
@@ -29,7 +33,7 @@ public class AboutFragment extends Fragment
     
     public AboutFragment(){}
     
-    private Activity mainActivity;
+    private Activity lMainActivity;
     private Button emailButton;
     private Button rateButton;
     private Button donateButton;
@@ -41,10 +45,10 @@ public class AboutFragment extends Fragment
 
         View rootView = inflater.inflate(R.layout.about_fragment, container,
                 false);
-        mainActivity = getActivity();
-        mainActivity.getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        lMainActivity = getActivity();
+        lMainActivity.getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         
-        final Resources res = mainActivity.getResources();
+        final Resources res = lMainActivity.getResources();
         
         emailButton = (Button) rootView.findViewById(R.id.email_button);
         emailButton.setOnClickListener(new OnClickListener()
@@ -52,7 +56,7 @@ public class AboutFragment extends Fragment
 			@Override
 			public void onClick(View v)
 			{
-				Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",mainActivity.getResources().getString(R.string.email), null));
+				Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto",lMainActivity.getResources().getString(R.string.email), null));
 				emailIntent.putExtra(Intent.EXTRA_SUBJECT, "CoffeeNerd");
 				startActivity(Intent.createChooser(emailIntent, "Send email..."));	
 			}
@@ -64,11 +68,11 @@ public class AboutFragment extends Fragment
 			@Override
 			public void onClick(View v)
 			{
-				Uri uri = Uri.parse("market://details?id=" + mainActivity.getPackageName());
+				Uri uri = Uri.parse("market://details?id=" + lMainActivity.getPackageName());
 			    Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
 			    try
 			    {
-			    	mainActivity.startActivity(goToMarket);
+			    	lMainActivity.startActivity(goToMarket);
 			    }
 			    catch (ActivityNotFoundException e)
 			    {
@@ -80,13 +84,38 @@ public class AboutFragment extends Fragment
         donateButton = (Button) rootView.findViewById(R.id.donate_button);
         donateButton.setOnClickListener(new OnClickListener()
         {
-			@Override
-			public void onClick(View v)
-			{
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.setData(Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=hashbangsoftware%40gmail%2ecom&lc=US&item_name=HashbangSoftware&currency_code=CAD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"));
-				startActivity(i);
-			}
+        	public void onClick(View v)
+            {
+                final Dialog dialog = new Dialog(lMainActivity);
+                dialog.setContentView(R.layout.support_popup);
+                dialog.setTitle("Support");
+                Button dialogButton = (Button) dialog.findViewById(R.id.paypal_button);
+                dialogButton.setOnClickListener(new OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+                        dialog.dismiss();
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=hashbangsoftware%40gmail%2ecom&lc=US&item_name=HashbangSoftware&currency_code=CAD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"));
+                        startActivity(i);
+                    }
+                });
+                
+                Button btcButton = (Button) dialog.findViewById(R.id.btc_button);
+                btcButton.setOnClickListener(new OnClickListener()
+                {
+                    public void onClick(View v)
+                    {
+                        dialog.dismiss();
+                        ClipboardManager clipboard = (ClipboardManager) lMainActivity.getSystemService(Context.CLIPBOARD_SERVICE); 
+                        ClipData clip = ClipData.newPlainText("wallet", "18DCG6e1GMQLvLdiCEZabxJhUPB9EREmsC");
+                        clipboard.setPrimaryClip(clip);
+                        Toast.makeText(lMainActivity, "Wallet address copied to your clipboard",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.show();    
+            }
 		});
         return rootView;
         
